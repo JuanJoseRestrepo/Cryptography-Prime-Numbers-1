1cf5be880aa279cd5978ccc53686ac5e3d60925a
Esteban Yusunguaira <zonitidus@gmail.com>
2020-02-02 19:16:41 -0500

    Luego miramos jsjs

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -1,5 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class MenuController {
-	
+
+	@FXML
+	private Button generate;
+	@FXML
+	private TextField n;
+
+	public void generatePrimes(ActionEvent e) {
+
+		int[] dim = squareUp();
+	}
+
+	public int[] squareUp() {
+
+		try {
+			int maxN = Integer.parseInt(n.getText());
+		} catch (Exception e) { // Excepción propia. Luego la hacemos
+			Alert a = new Alert(AlertType.ERROR);
+			a.setTitle("Error");
+			a.setContentText(e.getMessage());
+			a.show();
+		}
+
+		return null;
+	}
+
 }
---
+++ src/model/PrimeGenerator.java
@@ -0,0 +1,13 @@
+package model;
+
+public class PrimeGenerator {
+
+	public int[] squareUp(int n) {
+		return null;
+	}
+	
+	public int[][] generatePrimeNumbers() {
+		
+		return null;
+	}
+}

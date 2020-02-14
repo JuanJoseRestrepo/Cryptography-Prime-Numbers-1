48b1e8902610c3ef8e87aab62965ef120bdf3fe7
Isabella Valencia Correa <1116278698@academico.icesi.edu.co>
2020-02-14 13:28:31 -0500

    Matrix solution 3

--- src/Thread/ThreadPaint.java
+++ src/Thread/ThreadPaint.java
@@ -1,38 +1,51 @@
 package Thread;
 
 import controller.principalController;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import model.PrimeNumbers;
 
 public class ThreadPaint extends Thread {
 	
 	
-	private PrimeNumbers m;
-	private principalController m1;
-	private GridPane pane;
-	private int height;
+	private Label m;
+	private Boolean f;
+	private int sleept;
 	
-	
+	public ThreadPaint(Label m, Boolean f, int sleept) {
+		super();
+		this.m = m;
+		this.f = f;
+		this.sleept = sleept;
+	}
+
 	@Override
 	public void run() {
 		boolean t = true;
-		int contador = 1;
 		
-		
+
 		while(t) {
 			
-			if(m.isThatPrime(contador)) {
+			if(f == true) {
+				m.setTextFill(Color.web("#ff0000", 0.8));
+			}else {
+				
 			}
 			
-			
-			if(contador == height + 1) {
-				t = false;
+			try {
+				Thread.sleep(sleept);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
 		}
 		
 		
-	}
+	
 	
 
+}
+	
+	
 }
--- src/controller/principalController.java
+++ src/controller/principalController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import Thread.ThreadPaint;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -30,6 +32,8 @@ public class principalController implements Initializable {
 	private Button buton3;
 	@FXML
 	private TextField n;
+	@FXML
+	private BorderPane m;
 	
 	private GridPane juego;
 	
@@ -50,19 +54,20 @@ public class principalController implements Initializable {
 	public String generateMatrix(int number){
 		
 		String msj = "";
-		ArrayList<PrimeNumbers> array1 = createNumbers(number);
+		ArrayList<PrimeNumbers> array1 = new ArrayList<PrimeNumbers>();
 	
-		
 		double m = Math.sqrt(number);
+		int o = (int) m;
+		double resta = (m - o);
 		
-		double resta = (m - number);
-		
-		
+
 		if(principal.isThatPrime(number)) {
 			
-			m = m + 1;
+			int numero = (int) m;
 			
-			msj = m + "," + m;
+			numero = numero + 1;
+			
+			msj = numero + "," + numero;
 			
 		}else if(resta != 0.0) {
 			
@@ -77,7 +82,7 @@ public class principalController implements Initializable {
 				
 			}
 			
-			for(int i = 0; i < array1.size();i++) {
+			for(int i = 0; i < array1.size() - 1;i++) {
 				
 				int num1 = array1.get(i).getNumber();
 				int num2 = array1.get(i+1).getNumber();
@@ -91,7 +96,8 @@ public class principalController implements Initializable {
 			}
 			
 		}else {
-			msj = number + "," + number;
+			int y = (int) m ;
+			msj = y + "," + y;
 		}
 		
 		return msj;
@@ -124,9 +130,10 @@ public class principalController implements Initializable {
 		int high =  Integer.parseInt(n.getText());
 		String msj = generateMatrix(high);
 		String[] b = msj.split(",");
+		System.out.println(b[0]);
+		System.out.println(b[1]);
 		int fil = Integer.parseInt(b[0]);
 		int col = Integer.parseInt(b[1]);
-		
 		numbers = new PrimeNumbers[fil][col];
 		
 		
@@ -141,20 +148,33 @@ public class principalController implements Initializable {
         }
         
         int numero = 1;
-        
-        for(int i = 0; i < fil;i++) {
-        	
-        	for(int j = 0; j < col;j++) {
+        int time = 1000;
+        boolean t = false;
+        for(int i = 0; i < fil && !t;i++) {
+        	time += 3000;
+        	for(int j = 0; j < col && !t;j++) {
         		
         		String m1 = Integer.toString(numero);
         		Label numero1 = new Label(m1);
         		
-        		juego.add(numero1, j, i);
+        		ThreadPaint r = new ThreadPaint(numero1,principal.isThatPrime(numero),time);
+        		r.start();
+        	
         		numero++;
+        		time += 3000;
+        		juego.add(numero1, j, i);
+        		
+        		if(numero == high + 1) {
+        			t = true;
+        		}
         		
         	}
-        	numero = numero + 1;
+        	time += 3000;
         }
+       
+        
+        m.setCenter(juego);
+        
 		
 		}else {
 			Alert gameOver = new Alert(AlertType.INFORMATION);

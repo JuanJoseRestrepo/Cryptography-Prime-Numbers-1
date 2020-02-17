db3ba1549bd3ab7f3a5e32b1cd845f95ffd8c42b
Esteban Yusunguaira López <zonitidus@gmail.com>
2020-02-16 19:44:08 -0500

    Update principalController.java

--- src/controller/principalController.java
+++ src/controller/principalController.java
@@ -129,7 +129,7 @@ public class principalController implements Initializable {
 		        		r.start();
 		        	
 		        		numero++;
-		        		time += 15;
+		        		time += 40;
 		        		matrix.add(numero1, j, i);
 		        		j++;
 		
@@ -210,7 +210,7 @@ public class principalController implements Initializable {
 		        		r.start();
 		        	
 		        		numero++;
-		        		time += 15;
+		        		time += 40;
 		        		matrix.add(numero1, j, i);
 		        		j++;
 		
@@ -294,7 +294,7 @@ public class principalController implements Initializable {
 		        		r.start();
 		        	
 		        		numero++;
-		        		time += 15;
+		        		time += 40;
 		        		matrix.add(numero1, j, i);
 		        		j++;
 		
